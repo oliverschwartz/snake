5d28d5bd0ae7d789917346dfe1cbd711bfbd1633
Oliver Schwartz <ollieschwartz99@gmail.com>
2021-08-26 17:58:20 +1000

    replaced list with NonEmpty

--- app/Snake.hs
+++ app/Snake.hs
@@ -2,28 +2,29 @@
 
 module Snake where
 
+import qualified Data.List.NonEmpty as NE
+
 data Dir = U | D | L | R deriving (Eq, Show)
 data Piece = Piece Int Int deriving (Eq, Show)
-data Snake = Snake [Piece] deriving (Show)
+data Snake = Snake (NE.NonEmpty Piece) deriving (Show)
 data Food = Food Int Int deriving (Eq, Show)
 
 newSnake :: Snake
-newSnake = Snake [Piece 0 0]
+newSnake = Snake (Piece 0 0 NE.:| [])
 
 snakeContains :: Snake -> (Int, Int) -> Bool
 snakeContains (Snake pcs) (x,y) = elem (Piece x y) pcs
 
 -- Check the snake is valid (no overlaps and inside borders).
 validateSnake :: Snake -> Int -> Bool
-validateSnake s@(Snake pcs) boardSize = snakeCollision s && 
-                                        snakeInBorders s boardSize
+validateSnake s boardSize = snakeCollision s && snakeInBorders s boardSize
 
 snakeToCoors :: Snake -> [(Int, Int)]
-snakeToCoors (Snake []) = []
-snakeToCoors (Snake ((Piece x y):pcs)) = (x,y) : snakeToCoors (Snake pcs)
+snakeToCoors (Snake pcs) = NE.toList $ NE.map f pcs 
+    where f (Piece x y) = (x, y)
     
 snakeLen :: Snake -> Int
-snakeLen (Snake pcs) = length pcs
+snakeLen (Snake pcs) = NE.length pcs
 
 -- Check the snake is inside the borders.
 snakeInBorders :: Snake -> Int -> Bool
@@ -33,7 +34,7 @@ snakeInBorders (Snake pcs) boardSize = all f pcs where
 
 -- Check the snake isn't eating itself.
 snakeCollision :: Snake -> Bool
-snakeCollision (Snake pcs) = go pcs [] where
+snakeCollision (Snake pcs) = go (NE.toList pcs) [] where
     go lst acc = 
         case lst of 
             [] -> True
@@ -41,29 +42,28 @@ snakeCollision (Snake pcs) = go pcs [] where
 
 -- Move the snake along in the given direction. 
 moveSnake :: Snake -> Dir -> Snake
-moveSnake s@(Snake []) _ = s
-moveSnake (Snake (p:[])) dir = Snake [movePiece p dir]
-moveSnake (Snake (p:pcs)) dir = 
-    let newHead = movePiece p dir in 
-    let excludeLast = take (length pcs - 1) pcs in 
-    Snake (newHead : p : excludeLast)
+moveSnake (Snake (p NE.:| [])) dir = Snake (movePiece p dir NE.:| [])
+moveSnake (Snake (p NE.:| pcs)) dir = 
+    let newHead = movePiece p dir 
+        excludeLast = take (length pcs - 1) pcs
+        newTail = p : excludeLast
+    in Snake (newHead NE.:| newTail)
 
 -- Grow the snake. 
 growSnake :: Snake -> Dir -> Snake 
-growSnake (Snake []) _ = Snake []
-growSnake (Snake (hd@(Piece x y):[])) dir = 
+growSnake (Snake (hd@(Piece x y) NE.:| [])) dir = 
     let tl = case dir of 
             U -> Piece x (y-1)
             D -> Piece x (y+1)
             L -> Piece (x+1) y
             R -> Piece (x-1) y
-    in Snake [hd, tl]
+    in Snake (NE.fromList [hd, tl])
 growSnake (Snake pcs) _ = 
-    let len = length pcs
-        (Piece x y, Piece x' y') = (pcs !! (len-1), pcs !! (len-2))
+    let len = NE.length pcs
+        (Piece x y, Piece x' y') = (pcs NE.!! (len-1), pcs NE.!! (len-2))
         xDiff = x - x' 
         yDiff = y - y'
-    in Snake (pcs ++ [Piece (x + xDiff) (y + yDiff)])
+    in Snake $ NE.fromList (NE.toList pcs ++ [Piece (x + xDiff) (y + yDiff)])
 
 
 -- Move a piece along its direction if none provided. 
@@ -76,7 +76,9 @@ movePiece (Piece x y) dir = case dir of
 
 -- The smallest Manhattan distance from any piece of the snake.
 distFromSnake :: Snake -> (Int, Int) -> Int
-distFromSnake (Snake []) _ = maxBound :: Int
-distFromSnake (Snake ((Piece x y):pcs)) t@(x',y') = 
-    let dist = abs (x - x') + abs (y - y') in 
-    min dist (distFromSnake (Snake pcs) t)
+distFromSnake (Snake pcs) (x,y) = 
+    foldr f (maxBound :: Int) pcs 
+    where 
+        dist x' y' = abs (x - x') + abs (y - y')
+        f (Piece x' y') d = min (dist x' y') d
+    
